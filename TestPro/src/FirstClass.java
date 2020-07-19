74921a1753c7591fc98f25240a0c22813d0a5485
Anuruddha De Alwis <anuruddhadealwis@gmail.com>
2020-07-19 10:32:36 +1000

    Second class and the method added

--- TestPro/src/FirstClass.java
+++ TestPro/src/FirstClass.java
@@ -4,7 +4,11 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
 		System.out.println("This is the first class");
+		
+		SecondClass sec= new SecondClass();
+		sec.secondMethod();
 	}
 
 	
---
+++ TestPro/src/SecondClass.java
@@ -0,0 +1,9 @@
+
+public class SecondClass {
+
+	
+	public void secondMethod(){
+		System.out.println("This is the second method");
+	}
+	
+}
